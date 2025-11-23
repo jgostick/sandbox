6e2353ccdc203876adf0cbc88a810e619919d372
jgostick <jgostick@gmail.com>
2025-11-22 23:00:16 -0500

    Simplify version retrieval from pyproject.toml
    Removed metadata version retrieval and simplified version loading from pyproject.toml.

--- src/bump-my-version-sandbox/__init__.py
+++ src/bump-my-version-sandbox/__init__.py
@@ -1,13 +1,9 @@
-import importlib.metadata as _metadata
 import tomllib as _toml
 
 
-try:
-    __version__ = _metadata.version(__package__ or __name__)
-except _metadata.PackageNotFoundError:
-    with open("./pyproject.toml", "rb") as f:
-        data = _toml.load(f)
-        __version__ = data["project"]["version"]
+with open("./pyproject.toml", "rb") as f:
+    data = _toml.load(f)
+    __version__ = data["project"]["version"]
     
 
 from .main import *
